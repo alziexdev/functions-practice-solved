f78a5191e743f264973116f01567d2640ef6ac50
alziexdev <alziex@icloud.com>
2024-06-07 15:27:08 +0200

    Completed exercise 3

--- exercises/e3.js
+++ exercises/e3.js
@@ -16,8 +16,17 @@
 
 // Your code goes here...
 
+function doesArrayIncludeItemsBetweenVals(arr, val1, val2) {
+  for (let elem of arr) {
+    if (elem > val1 && elem < val2) {
+      return true;
+    }
+  }
+  return false;
+}
 
-
+doesArrayIncludeItemsBetweenVals([2, 4, 2], 3, 5);
+doesArrayIncludeItemsBetweenVals([2, 4, 2], 5, 10);
 
 /**
  * ====================================================
@@ -36,8 +45,38 @@
 
 // Your code goes here...
 
+function getValueWithConditionOne(num1, num2) {
+  if (num1 == 40 && num2 == 40) {
+    return num1 + num2;
+  } else { 
+    return (num1 + num2) * 2; 
+  }
+}
 
+getValueWithConditionOne(40, 40);
+getValueWithConditionOne(20, 30);
 
+const getValueWithConditionTwo = (num1, num2) => {
+  if (num1 == 40 && num2 == 40) {
+    return num1 + num2;
+  } else { 
+    return (num1 + num2) * 2; 
+  }
+}
+
+getValueWithConditionTwo(40, 40);
+getValueWithConditionTwo(20, 30);
+
+const getValueWithConditionThree = function(num1, num2) {
+  if (num1 == 40 && num2 == 40) {
+    return num1 + num2;
+  } else { 
+    return (num1 + num2) * 2; 
+  }
+}
+
+getValueWithConditionThree(40, 40);
+getValueWithConditionThree(20, 30);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-3"
